157d7129c1e351d625dd43f0b6ce4f11d6acf3d2
yuto YAMAGUCHI <23238474@edu.cc.saga-u.ac.jp>
2025-07-10 01:25:39 +0900

    トップページの土台作成

---
+++ fuel/app/config/crypt.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+  'crypto_key' => 'omYlYcVtUPasL3AqowRD4haQ',
+  'crypto_iv' => 'CIMCI0MRQuU4TTkM-o5gUd3Y',
+  'crypto_hmac' => 'rUQTyQeOMvjI2P4o2YM0IWFo',
+);
---
+++ fuel/app/logs/2025/07/10.php
@@ -0,0 +1,14 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-07-10 00:06:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-07-10 00:11:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-07-10 00:13:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-07-10 00:15:40 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-07-10 00:50:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-07-10 01:12:39 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-07-10 01:12:39 --> Fatal Error - Method Fuel\Core\View::__toString() must not throw an exception, caught Error: Call to undefined method Fuel\Core\Html::chars() in /var/www/html/my_fuel_project/fuel/core/classes/response.php on line 0
+WARNING - 2025-07-10 01:16:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2025-07-10 01:16:48 --> Notice - Undefined variable: current_user in /var/www/html/my_fuel_project/fuel/app/views/welcome/index.php on line 208
+WARNING - 2025-07-10 01:17:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-07-10 01:17:27 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-07-10 01:18:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
